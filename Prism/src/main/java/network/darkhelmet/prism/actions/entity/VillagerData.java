548f093b3d89c095e039a0e01af10e9cd8e426d7
Envel <Envel@envel.envel>
2023-07-06 15:55:16 +0300

    1

---
+++ Prism/src/main/java/network/darkhelmet/prism/actions/entity/VillagerData.java
@@ -0,0 +1,17 @@
+package network.darkhelmet.prism.actions.entity;
+
+public class VillagerData {
+    private String tradesJson;
+
+    public VillagerData(String tradesJson) {
+        this.tradesJson = tradesJson;
+    }
+
+    public String getTradesJson() {
+        return tradesJson;
+    }
+
+    public void setTradesJson(String tradesJson) {
+        this.tradesJson = tradesJson;
+    }
+}
